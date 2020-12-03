39162a0d03487f2aa3af25a7b6362bc165e7fcf8
alextinng <alextinng@icloud.com>
2020-12-03 23:32:39 +0800

    feat(problem): add solution.
    - add solution of question 9.

--- src/main/java/dev/alex/leetcode/_7.java
+++ src/main/java/dev/alex/leetcode/_7.java
@@ -1,6 +1,6 @@
 package dev.alex.leetcode;
 
-class _7 {
+public class _7 {
     public int reverse(int x) {
         for (long n = 0L; x != 0; x = x / 10) {
             n = n * 10 + x % 10;
@@ -10,11 +10,6 @@ class _7 {
         }
 
         return 0;
-    }
-
-    public static void main(String[] args) {
-        _7 instance = new _7();
-        System.out.println(instance.reverse(-2147483648));
     }
 }
 
---
+++ src/main/java/dev/alex/leetcode/_9.java
@@ -0,0 +1,18 @@
+package dev.alex.leetcode;
+
+public class _9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else {
+            char[] digits = Integer.toString(x).toCharArray();
+            for (int i = 0, j = digits.length - i - 1; j - i >= 1; i++, j--) {
+                if (digits[i] != digits[j]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
