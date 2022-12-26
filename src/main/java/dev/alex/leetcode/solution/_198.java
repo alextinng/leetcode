003e0ba08bce0be4d53b333fe24f1190fb23e7d3
AlexTing <alextinng@hotmail.com>
2022-12-26 15:12:21 +0800

    add solution

--- src/main/java/dev/alex/leetcode/solution/_198.java
+++ src/main/java/dev/alex/leetcode/solution/_198.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class _198 {
     public int rob(int[] nums) {
-        return solution1(nums);
+        return solution3(nums);
     }
 
     private int solution1(int[] nums) {
@@ -38,5 +38,17 @@ public class _198 {
         }
 
         return result;
+    }
+
+    private int solution3(int[] nums) {
+        int[] dp = new int[nums.length];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+
+        return dp[nums.length - 1];
     }
 }
